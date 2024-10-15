71cecb6b655a1d79e7bd67d3a1aa5810d895b398
eleneaa <lenatisenko4@gmail.com>
2024-10-15 13:02:38 +0500

    application delete

--- src/skillbox/mod1/ContactService.java
+++
@@ -1,9 +0,0 @@
-package skillbox.mod1;
-
-public interface ContactService {
-    void addContact(Contact contact);
-    void deleteContact(String email);
-    void displayAllContacts();
-    void saveContactsToFile();
-
-}
